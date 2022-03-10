a0069ef93042393c1045c0ab298566d1ad4ac25a
itookyourboo <wignorbo@yandex.ru>
2022-03-10 14:43:47 +0300

    additional task

--- lab3_integration/integral_solver.py
+++ lab3_integration/integral_solver.py
@@ -11,11 +11,11 @@ def rect_method(f, a, b, align=0, num=100):
     h = (b - a) / num
     arr = np.linspace(a, b, num, endpoint=False)
     match align:
-        case -1:
+        case RectAlign.LEFT:
             res = h * sum(f(x) for x in arr)
-        case 0:
+        case RectAlign.MID:
             res = sum(h * f(x + h / 2) for x in arr)
-        case 1:
+        case RectAlign.RIGHT:
             res = sum(h * f(x + h) for x in arr)
         case _:
             raise ValueError('incorrect align')
@@ -31,3 +31,43 @@ def trapeze_method(f, a, b, num=100):
 
 def runge_err(res, res2, k):
     return abs(res2 - res) / (2 ** k - 1)
+
+
+def inf_or_err(f, num):
+    try:
+        res = f(num)
+        return np.isinf(res)
+    except Exception:
+        return True
+
+
+def diverges(f, a, b):
+    return inf_or_err(f, a) or inf_or_err(f, b)
+
+
+def normalized_rect_method(f, F, a, b, align=0, num=100):
+    if diverges(F, a, b):
+        return None
+
+    try:
+        res = rect_method(f, a, b, align, num)
+        if np.isinf(res):
+            return None
+    except:
+        return None
+
+    return res
+
+
+def normalized_trapeze_method(f, F, a, b, num=100):
+    if diverges(F, a, b):
+        return None
+
+    try:
+        res = trapeze_method(f, a, b, num)
+        if np.isinf(res):
+            return None
+    except:
+        return None
+
+    return res
--- lab3_integration/main.py
+++ lab3_integration/main.py
@@ -1,12 +1,21 @@
-from integral_solver import rect_method, trapeze_method, runge_err
+import math
 
-f = lambda x: -x ** 3 - x ** 2 - 2 * x + 1
-k = 2
-f_str = "-x^3 - x^2 - 2x + 1"
-I_EXACT = -26 / 3
+from integral_solver import (
+    rect_method,
+    trapeze_method,
+    runge_err,
+    normalized_rect_method,
+    normalized_trapeze_method
+)
 
 
 def main():
+    F = lambda x: -x ** 4 / 4 - x ** 3 / 3 - x ** 2 + x
+    f = lambda x: -x ** 3 - x ** 2 - 2 * x + 1
+    k = 2
+    f_str = "-x^3 - x^2 - 2x + 1"
+    I_EXACT = -26 / 3
+
     print(f'''Выберите метод для вычисления интеграла функции: {f_str}
     1. Метод левых прямоугольников
     2. Метод средних прямоугольников
@@ -42,7 +51,75 @@ def main():
     res2 = err_f()
     err = runge_err(res, res2, k)
     print(f'Погрешность по правилу Рунге: {err:.4f}')
+    print('По методу Ньютона-Лейбница:', F(b) - F(a))
+
+
+def get_end_result(result):
+    if not isinstance(result, tuple):
+        return result
+
+    status, res = result
+    if status:
+        return res
+
+    return get_end_result(res)
+
+
+def add():
+    functions = [
+        {
+            'f': lambda x: 1 / x,
+            'F': lambda x: math.log(x),
+            's': '1 / x'
+        },
+        {
+            'f': lambda x: 1 / (3 - x) ** 0.5,
+            'F': lambda x: -2 * (3 - x) ** 0.5,
+            's': '1 / sqrt(3 - x)'
+        },
+    ]
+
+    print('Выберите функцию для интегрирования:')
+    for i, fun in enumerate(functions, 1):
+        print(f"{i}. {fun['s']}")
+
+    fun_i = int(input())
+    if not 1 <= fun_i <= len(functions):
+        print('Неверное значение')
+        add()
+
+    fun = functions[fun_i - 1]
+    f, F, s = fun['f'], fun['F'], fun['s']
+
+    print(f'''Выберите метод для вычисления интеграла функции: {fun["s"]}
+        1. Метод левых прямоугольников
+        2. Метод средних прямоугольников
+        3. Метод правых прямоугольников
+        4. Метод трапеций'''
+          )
+
+    method = int(input())
+    if not 1 <= method <= 4:
+        print('Неверное значение')
+        add()
+
+    a, b = map(float, input('Введите границы интегрирования через пробел: ').split())
+    n = int(input('Введите число разбиений: '))
+    fun = None
+
+    match method:
+        case 1 | 2 | 3:
+            fun = lambda: normalized_rect_method(f, F, a, b, method - 2, n)
+        case 4:
+            fun = lambda: normalized_trapeze_method(f, F, a, b, n)
+
+    res = fun()
+    if res is None:
+        print('Интеграл расходится')
+    else:
+        print('Результат вычисления:', res)
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    add()
