055a9d780290f4b0995505a88b01a56131089ef3
itookyourboo <wignorbo@yandex.ru>
2022-05-05 13:41:03 +0300

    fixed adams method error

--- lab6_derivative/methods.py
+++ lab6_derivative/methods.py
@@ -11,20 +11,21 @@ def improved_euler_method(eq, h):
 
 def adams_method(eq, h):
     n = int((eq.b - eq.a) / h)
-    x, y = [eq.a], [eq.y0]
-    for i in range(n):
-        x.append(x[i] + h)
-        y.append(y[i] + h * eq.dif(x[i], y[i]))
+    x, y = improved_euler_method(eq, h)
+    # x, y = [eq.a], [eq.y0]
+    # for i in range(n):
+    #     x.append(x[i] + h)
+    #     y.append(y[i] + h * eq.dif(x[i], y[i]))
 
     f = eq.dif
-    for i in range(3, n + 1):
+    for i in range(3, len(x)):
         k = [f(x[i - q], y[i - q]) for q in range(4)]
         df = k[0] - k[1]
         d2f = k[0] - 2 * k[1] + k[2]
         d3f = k[0] - 3 * k[1] + 3 * k[2] - k[3]
         y[i] = (
             y[i - 1] +
-            1 * h ** 1 * k[0] +
+            1 * h ** 1 * k[1] +
             1 * h ** 2 * df / 2 +
             5 * h ** 3 * d2f / 12 +
             3 * h ** 4 * d3f / 8
